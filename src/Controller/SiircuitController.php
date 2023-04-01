cb910e2c2ea3983300a7fc6124becd6600c97368
Tommy Guy <tommyguy65@gmail.com>
2023-04-01 19:56:47 +0200

    Generation images sur la page circuit

--- src/Controller/SiircuitController.php
+++ src/Controller/SiircuitController.php
@@ -11,8 +11,35 @@ class SiircuitController extends AbstractController
     #[Route('/siircuit', name: 'app_siircuit')]
     public function index(): Response
     {
+        $folderOffi = './assets/images/races/officielles';
+        $folderOption = './assets/images/races/optionnelles';
+
+
+        $filesOffi = scandir($folderOffi);
+        $filesOption = scandir($folderOption);
+
+        // Parcourez les fichiers et affichez-les un par un
+        $imagesOffi = [];
+        $imagesOption = [];
+        foreach($filesOffi as $file) {
+            if($file != "." && $file != "..") { // Ignorez les fichiers "." et ".." qui représentent le dossier courant et le dossier parent
+                $imagesOffi []= $file;
+            }
+        }
+
+        foreach($filesOption as $file) {
+            if($file != "." && $file != "..") { // Ignorez les fichiers "." et ".." qui représentent le dossier courant et le dossier parent
+                $imagesOption []= $file;
+            }
+        }
+
+
+
+
         return $this->render('siircuit/index.html.twig', [
             'controller_name' => 'SiircuitController',
+            'images_circuit_offi' => $imagesOffi,
+            'images_circuit_option' => $imagesOption
         ]);
     }
 }

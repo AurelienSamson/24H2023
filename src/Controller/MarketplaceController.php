f94965a671fe5a760e01ecfe6ed6f00c1022f39d
Maudulo <ma.be92@hotmail.fr>
2023-04-01 21:37:23 +0200

    WIP marketplace affichage objets

--- src/Controller/MarketplaceController.php
+++ src/Controller/MarketplaceController.php
@@ -11,6 +11,58 @@ class MarketplaceController extends AbstractController
     #[Route('/marketplace', name: 'app_marketplace')]
     public function index(): Response
     {
+        $folderBrakes = './assets/images/items/brakes';
+        $filesBrakes = scandir($folderBrakes);
+
+        $folderKits = './assets/images/items/kits';
+        $filesKits = scandir($folderKits);
+
+        $folderMotors = './assets/images/items/motors';
+        $filesMotors = scandir($folderMotors);
+
+        $folderSpoilers = './assets/images/items/spoilers';
+        $filesSpoilers = scandir($folderSpoilers);
+
+        $folderWheels = './assets/images/items/wheels';
+        $filesWheels = scandir($folderWheels);
+
+        // Parcourez les fichiers et affichez-les un par un
+        $imageObjetsBrakes = [];
+        $imageObjetsKits = [];
+        $imageObjetsMotors = [];
+        $imageObjetsSpoilers = [];
+        $imageObjetsWheels = [];
+
+        foreach($filesBrakes as $file) {
+            if($file != "." && $file != "..") { // Ignorez les fichiers "." et ".." qui représentent le dossier courant et le dossier parent
+                $imageObjetsBrakes []= $file;
+            }
+        }
+
+        foreach($filesKits as $file) {
+            if($file != "." && $file != "..") { // Ignorez les fichiers "." et ".." qui représentent le dossier courant et le dossier parent
+                $imageObjetsKits []= $file;
+            }
+        }
+
+        foreach($filesMotors as $file) {
+            if($file != "." && $file != "..") { // Ignorez les fichiers "." et ".." qui représentent le dossier courant et le dossier parent
+                $imageObjetsMotors []= $file;
+            }
+        }
+
+        foreach($filesSpoilers as $file) {
+            if($file != "." && $file != "..") { // Ignorez les fichiers "." et ".." qui représentent le dossier courant et le dossier parent
+                $imageObjetsSpoilers []= $file;
+            }
+        }
+
+        foreach($filesWheels as $file) {
+            if($file != "." && $file != "..") { // Ignorez les fichiers "." et ".." qui représentent le dossier courant et le dossier parent
+                $imageObjetsWheels []= $file;
+            }
+        }
+
         return $this->render('marketplace/index.html.twig', [
             "statsVoiture"=> [
                 ["puissance", 2],
@@ -21,6 +73,7 @@ class MarketplaceController extends AbstractController
                 ["usure", 0],
                 ["poids", 0],
             ],
+
             "statsVoitureModifie"=> [
                 ["puissance", 1],
                 ["accélération", 0],
@@ -29,7 +82,38 @@ class MarketplaceController extends AbstractController
                 ["consommation", 0],
                 ["usure", 0],
                 ["poids", 0],
-            ]
+            ],
+            /*$this->render('siircuit/index.html.twig', [
+                'controller_name' => 'SiircuitController',
+                'images_objets' => $imageObjets
+            ])*/
         ]);
     }
 }
+
+
+
+
+$folderOffi = './assets/images/races/officielles';
+        $folderOption = './assets/images/races/optionnelles';
+
+
+        $filesOffi = scandir($folderOffi);
+        $filesOption = scandir($folderOption);
+
+        // Parcourez les fichiers et affichez-les un par un
+        $imagesOffi = [];
+        $imagesOption = [];
+        foreach($filesOffi as $file) {
+            if($file != "." && $file != "..") { // Ignorez les fichiers "." et ".." qui représentent le dossier courant et le dossier parent
+                $imagesOffi []= $file;
+            }
+        }
+
+        foreach($filesOption as $file) {
+            if($file != "." && $file != "..") { // Ignorez les fichiers "." et ".." qui représentent le dossier courant et le dossier parent
+                $imagesOption []= $file;
+            }
+        }
+
+        return ;
